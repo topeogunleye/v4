dc51dc0a68ef91d7559439ec7998e44c1c273ac7
“Temitope <topeogunleye@gmail.com>
2023-12-26 12:30:17 +0100

    Modify logo

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,35 +1,27 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-      <path
+    <g>
+      <g id="B" transform="translate(11.000000, 5.000000)">
+        <path
           d="M 54 36 L 27 36 L 27 30 L 54 30 Z Z M 37 60 L 44 60 L 44 36 L 37 36 Z"
           fill="currentColor"
         />
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-        <path
+      </g>
+      <path
         stroke="currentColor"
         strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M 50, 5
-        L 11, 27
-        L 11, 72
-        L 50, 95
-        L 89, 73
-        L 89, 28 z"
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
       />
-      </g>
     </g>
   </svg>
 );
